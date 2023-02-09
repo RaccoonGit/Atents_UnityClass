c7a9e59b7cfc7a80530bd4b6167b949734cb84fe
RaccoonCode <74027129+RaccoonGit@users.noreply.github.com>
2023-02-09 20:52:27 +0900

    add/zombie damage script

---
+++ FirstPersonUnity/Assets/02.Scripts/ZombieDamage.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieDamage : MonoBehaviour
+{
+
+    #region this.Components
+    #endregion
+    #region Public Property
+    #endregion
+
+    /***********************************************************************
+    *                             Unity Events
+    ***********************************************************************/
+    #region Unity Events
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    /***********************************************************************
+    *                            Private Methods
+    ***********************************************************************/
+    #region Private Methods
+    #endregion
+}
